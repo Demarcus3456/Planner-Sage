328a96ea3bee9b1c8fcf18bc2baa72220521649a
“Demarcus <dcolemand@ymail.com>
2023-09-14 08:44:01 -0500

    Card flip

--- fun/script.js
+++ fun/script.js
@@ -2,8 +2,21 @@
 
 const menu = document.querySelector('.menu');
 const navBar = document.querySelector('.navbar');
+const cardFront = document.querySelector('.layout');
+const cardBack = document.querySelector('.card-back');
+const info = document.querySelector('.info');
 
+// Opens menu
 
 menu.addEventListener('click', function () {
     navBar.classList.remove('hidden');
 });
+
+// Card flip
+
+info.addEventListener('click', function () {
+    cardFront.classList.add('rotate');
+    cardBack.classList.remove('rotate');
+    cardFront.classList.add('trastion');
+    cardBack.classList.add('trastion');
+})
