a5d2f64238d944969af319bebe5b9bba9da812f4
“Demarcus <dcolemand@ymail.com>
2023-09-17 17:18:05 -0500

    Update
    Updated the timer

--- personal/script.js
+++ personal/script.js
@@ -2,8 +2,17 @@
 
 const urgeBtn = document.querySelector('.urge-btn')
 const urgeContainer = document.querySelector('.urge');
+const counter = document.querySelector('.counter');
+const back = document.querySelector('.x');
 
 // Opens urge box
 urgeBtn.addEventListener('click', function () {
     urgeContainer.classList.remove('hidden');
+    counter.classList.add('hidden');
+});
+
+// Closes urge box
+back.addEventListener('click', function () {
+    urgeContainer.classList.add('hidden');
+    counter.classList.remove('hidden');
 });
