88073642b9bd29ea447fafd48236e45d3a7b3142
“Demarcus <dcolemand@ymail.com>
2023-10-15 18:53:43 -0500

    career
    Updated page

--- career/script.js
+++ career/script.js
@@ -1,1 +1,9 @@
 'use strict'
+
+const navbarBtn = document.querySelector('.navbar-btn');
+const navbar = document.querySelector('.navbar');
+
+// Toggles menu
+navbarBtn.addEventListener('click', function () {
+    navbar.classList.toggle('hidden');
+});
