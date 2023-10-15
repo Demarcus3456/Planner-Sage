65943578f18f08349d5014f7a267b58c46960dbd
“Demarcus <dcolemand@ymail.com>
2023-10-15 13:30:56 -0500

    Bug
    Fixed again

--- social/script.js
+++ social/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const lessons = document.querySelector('.lessons');
+const lessons = document.querySelector('.container');
+const start = document.querySelector('.start');
+const noteBtn = document.querySelector('.note');
+const notes = document.querySelector('.notes');
 
 // Resonator Buttons
 const resonatorLessons = document.querySelector('.vocal');
@@ -8,6 +11,16 @@ const partsMenu = document.querySelector('.part');
 const partsItems = document.querySelector('.part-items');
 const part1Btn = document.querySelector('.p-1');
 const part1 = document.querySelector('.part-1');
+
+// Starts Lessons
+start.addEventListener('click', function () {
+    lessons.classList.remove('hidden');
+});
+
+// Opens notes
+noteBtn.addEventListener('click', function () {
+    notes.classList.remove('hidden');
+});
 
 // Opens resonator section
 resonatorLessons.addEventListener('click', function () {
@@ -17,10 +30,11 @@ resonatorLessons.addEventListener('click', function () {
 
 // Opens parts menu
 partsMenu.addEventListener('click', function () {
-    partsItems.classList.remove('hidden');
+    partsItems.classList.toggle('hidden');
 });
 
 // Opens part 1
 part1Btn.addEventListener('click', function () {
     part1.classList.remove('hidden');
+    partsItems.classList.add('hidden');
 })
