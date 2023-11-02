d4f0f208161ee8577d37eb872364e42379e4f012
Demarcus Mcneese <dcolemand@ymail.com>
2023-11-02 14:22:48 -0500

    I finish adding all gym videos

--- social/gym/script.js
+++ social/gym/script.js
@@ -344,10 +344,10 @@ part11.addEventListener('click', function () {
     }
 });
 
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+part12.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/d3vokci6ev7cqw2e2l7k4/12.-Triceps-Training.mp4?rlkey=upv8lm5ofxrblfi2xjx4lkman&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/d3vokci6ev7cqw2e2l7k4/12.-Triceps-Training.mp4?rlkey=upv8lm5ofxrblfi2xjx4lkman&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -358,7 +358,7 @@ part1.addEventListener('click', function () {
         part9.classList.remove('active');
         part10.classList.remove('active');
         part11.classList.remove('active');
-        part12.classList.remove('active');
+        part12.classList.add('active');
         part13.classList.remove('active');
         part14.classList.remove('active');
         part15.classList.remove('active');
@@ -372,10 +372,11 @@ part1.addEventListener('click', function () {
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part13.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/ibwbm0avd1j1t4fpy0kzp/13.-Biceps-Training.mp4?rlkey=gvt0v61zq8efy1p4no4jzedp1&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/ibwbm0avd1j1t4fpy0kzp/13.-Biceps-Training.mp4?rlkey=gvt0v61zq8efy1p4no4jzedp1&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -387,7 +388,7 @@ part1.addEventListener('click', function () {
         part10.classList.remove('active');
         part11.classList.remove('active');
         part12.classList.remove('active');
-        part13.classList.remove('active');
+        part13.classList.add('active');
         part14.classList.remove('active');
         part15.classList.remove('active');
         part16.classList.remove('active');
@@ -400,10 +401,11 @@ part1.addEventListener('click', function () {
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part14.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/sh4snuwp6rvey5iebbi62/14.-Free-weights-Machines-and-Compound-Movements.mp4?rlkey=nrzf1s3s5d32v5g7j5h9i0s1t&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/sh4snuwp6rvey5iebbi62/14.-Free-weights-Machines-and-Compound-Movements.mp4?rlkey=nrzf1s3s5d32v5g7j5h9i0s1t&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -416,7 +418,7 @@ part1.addEventListener('click', function () {
         part11.classList.remove('active');
         part12.classList.remove('active');
         part13.classList.remove('active');
-        part14.classList.remove('active');
+        part14.classList.add('active');
         part15.classList.remove('active');
         part16.classList.remove('active');
         part17.classList.remove('active');
@@ -428,10 +430,11 @@ part1.addEventListener('click', function () {
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part15.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/g1cgl4jucesxql22rrkbr/15.-Starting-Training-for-Newbies.mp4?rlkey=rf0udq9c88c8sxrvxcxnkobpd&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/g1cgl4jucesxql22rrkbr/15.-Starting-Training-for-Newbies.mp4?rlkey=rf0udq9c88c8sxrvxcxnkobpd&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -445,7 +448,7 @@ part1.addEventListener('click', function () {
         part12.classList.remove('active');
         part13.classList.remove('active');
         part14.classList.remove('active');
-        part15.classList.remove('active');
+        part15.classList.add('active');
         part16.classList.remove('active');
         part17.classList.remove('active');
         part18.classList.remove('active');
@@ -456,10 +459,11 @@ part1.addEventListener('click', function () {
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part16.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/qgb9nc4nc5d57s6yypn05/16.-Diet-and-Protein-Introduction.mp4?rlkey=53vdib67fgylr47z13gdjl8u1&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/qgb9nc4nc5d57s6yypn05/16.-Diet-and-Protein-Introduction.mp4?rlkey=53vdib67fgylr47z13gdjl8u1&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -474,7 +478,7 @@ part1.addEventListener('click', function () {
         part13.classList.remove('active');
         part14.classList.remove('active');
         part15.classList.remove('active');
-        part16.classList.remove('active');
+        part16.classList.add('active');
         part17.classList.remove('active');
         part18.classList.remove('active');
         part19.classList.remove('active');
@@ -484,10 +488,11 @@ part1.addEventListener('click', function () {
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part17.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/s7if3tj986qfex42da09b/17.-Fats-and-Carbs.mp4?rlkey=m0byvwc4n2bthwhc7ay2g2kgk&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/s7if3tj986qfex42da09b/17.-Fats-and-Carbs.mp4?rlkey=m0byvwc4n2bthwhc7ay2g2kgk&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -503,7 +508,7 @@ part1.addEventListener('click', function () {
         part14.classList.remove('active');
         part15.classList.remove('active');
         part16.classList.remove('active');
-        part17.classList.remove('active');
+        part17.classList.add('active');
         part18.classList.remove('active');
         part19.classList.remove('active');
         part20.classList.remove('active');
@@ -512,10 +517,11 @@ part1.addEventListener('click', function () {
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part18.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/4b9xqacs9otesvt36goiy/18.-Calories-and-Weight-Manipulation.mp4?rlkey=t67m1kdqnxueoh6b0qe8mvk19&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/4b9xqacs9otesvt36goiy/18.-Calories-and-Weight-Manipulation.mp4?rlkey=t67m1kdqnxueoh6b0qe8mvk19&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -532,7 +538,7 @@ part1.addEventListener('click', function () {
         part15.classList.remove('active');
         part16.classList.remove('active');
         part17.classList.remove('active');
-        part18.classList.remove('active');
+        part18.classList.add('active');
         part19.classList.remove('active');
         part20.classList.remove('active');
         part21.classList.remove('active');
@@ -540,10 +546,11 @@ part1.addEventListener('click', function () {
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part19.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/uscv16ut1o0nfusfbi61z/19.-How-to-Count-Calories.mp4?rlkey=qjqynzrr8la3snms24ix9orxj&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/uscv16ut1o0nfusfbi61z/19.-How-to-Count-Calories.mp4?rlkey=qjqynzrr8la3snms24ix9orxj&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -561,17 +568,18 @@ part1.addEventListener('click', function () {
         part16.classList.remove('active');
         part17.classList.remove('active');
         part18.classList.remove('active');
-        part19.classList.remove('active');
+        part19.classList.add('active');
         part20.classList.remove('active');
         part21.classList.remove('active');
         part22.classList.remove('active');
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part20.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/t1v5grwhzivgfng8tl23a/20.-Fruit-and-Vegetables.mp4?rlkey=rtmr1gtsuv93zz2agd0kqzdx6&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/t1v5grwhzivgfng8tl23a/20.-Fruit-and-Vegetables.mp4?rlkey=rtmr1gtsuv93zz2agd0kqzdx6&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -590,16 +598,17 @@ part1.addEventListener('click', function () {
         part17.classList.remove('active');
         part18.classList.remove('active');
         part19.classList.remove('active');
-        part20.classList.remove('active');
+        part20.classList.add('active');
         part21.classList.remove('active');
         part22.classList.remove('active');
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part21.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/dst4jgqpfwdrzi2d5f5ay/21.-Supplements.mp4?rlkey=skd99rcuccylhlp97vaojhnio&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/dst4jgqpfwdrzi2d5f5ay/21.-Supplements.mp4?rlkey=skd99rcuccylhlp97vaojhnio&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -619,15 +628,16 @@ part1.addEventListener('click', function () {
         part18.classList.remove('active');
         part19.classList.remove('active');
         part20.classList.remove('active');
-        part21.classList.remove('active');
+        part21.classList.add('active');
         part22.classList.remove('active');
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part22.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/hsjix9kiqe7i9do5da48p/22.-Your-Journey-Bulking-and-Cutting-Phases.mp4?rlkey=c2x17jizka3derphcj0jyqmoa&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/hsjix9kiqe7i9do5da48p/22.-Your-Journey-Bulking-and-Cutting-Phases.mp4?rlkey=c2x17jizka3derphcj0jyqmoa&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -648,14 +658,15 @@ part1.addEventListener('click', function () {
         part19.classList.remove('active');
         part20.classList.remove('active');
         part21.classList.remove('active');
-        part22.classList.remove('active');
+        part22.classList.add('active');
         part23.classList.remove('active');
     }
 });
-part1.addEventListener('click', function () {
-    mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1';
-    if (mainVideo.src = 'https://www.dropbox.com/s/l1y0o6cn7nx20c5/1.%20Introduction%20to%20Gym%20Unleashed.mp4?raw=1') {
-        part1.classList.add('active');
+
+part23.addEventListener('click', function () {
+    mainVideo.src = 'https://www.dropbox.com/scl/fi/qdxg1ix0ck4bzqqypfn6s/23.-Common-Questions-and-Answers.mp4?rlkey=c2yf00cppjy8l8zo0e7hlsnbn&raw=1';
+    if (mainVideo.src = 'https://www.dropbox.com/scl/fi/qdxg1ix0ck4bzqqypfn6s/23.-Common-Questions-and-Answers.mp4?rlkey=c2yf00cppjy8l8zo0e7hlsnbn&raw=1') {
+        part1.classList.remove('active');
         part2.classList.remove('active');
         part3.classList.remove('active');
         part4.classList.remove('active');
@@ -677,6 +688,6 @@ part1.addEventListener('click', function () {
         part20.classList.remove('active');
         part21.classList.remove('active');
         part22.classList.remove('active');
-        part23.classList.remove('active');
+        part23.classList.add('active');
     }
 });
